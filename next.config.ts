87002fa2d9c2907ba5dba0e424aeba94b0aa9638
Aareen <anandaareen@gmail.com>
2025-10-16 17:02:43 +0000

    make changes to previous before production

--- next.config.ts
+++ next.config.ts
@@ -18,8 +18,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https'
-        ,
+        protocol: 'https',
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
