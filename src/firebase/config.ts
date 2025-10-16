f9b7e78f7f3ca33ec1faf68c02c3a7efea9e9802
Aareen <anandaareen@gmail.com>
2025-10-16 19:07:26 +0000

    hi

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,4 +1,7 @@
 
+// For debugging Vercel environment variables
+console.log("Attempting to use Firebase API Key:", process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
+
 export const firebaseConfig = {
   projectId: 'studio-6796978752-8c716',
   appId: '1:363506330731:web:2bd69d0ce84a4c864a952e',
